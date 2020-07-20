024ff4656d0122af0ac7a43acbc5ef358e20ac74
Aaron <xzy2967@163.com>
2020-07-20 14:56:07 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package com.example.springcloud.controller;
+
+import com.example.springcloud.entities.CommonResult;
+import com.example.springcloud.entities.Payment;
+import com.example.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("******插入结果："+result);
+
+        if (result>0){
+            return new CommonResult(200,"插入数据库成功",result);
+        }else {
+            return new CommonResult(444,"插入数据库失败");
+        }
+
+    }
+
+    @GetMapping(value = "/payment/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("查询结果："+payment);
+
+        if(payment!=null){
+            return new CommonResult<Payment>(200,"查询成果",payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询id："+id);
+        }
+
+    }
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.dao;
+
+import com.example.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param(("id")) long id);
+
+}
